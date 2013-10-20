67506600aa1f63a6aaaa7d8e9ceccaba6b528217
Miroslav Magda <magda.miroslav@gmail.com>
2013-10-20 15:47:17 +0200

    Constants shared across extension

---
+++ extension/common/const.js
@@ -0,0 +1,63 @@
+/**
+ * Constants shared across extension
+ * TODO: build process to include constant in *.js
+ */
+
+var CONST = {};
+/**
+ * EQ default values
+ */
+CONST.EQ = [{
+    label : 'master',
+    gain : 1
+}, {
+    label : '32',
+    f : 32,
+    gain : 0,
+    type : 3
+}, {
+    label : '64',
+    f : 64,
+    gain : 0,
+    type : 5
+}, {
+    label : '125',
+    f : 125,
+    gain : 0,
+    type : 5
+}, {
+    label : '250',
+    f : 250,
+    gain : 0,
+    type : 5
+}, {
+    label : '500',
+    f : 500,
+    gain : 0,
+    type : 5
+}, {
+    label : '1k',
+    f : 1000,
+    gain : 0,
+    type : 5
+}, {
+    label : '2k',
+    f : 2000,
+    gain : 0,
+    type : 5
+}, {
+    label : '4k',
+    f : 4000,
+    gain : 0,
+    type : 5
+}, {
+    label : '8k',
+    f : 8000,
+    gain : 0,
+    type : 5
+}, {
+    label : '16k',
+    f : 16000,
+    gain : 0,
+    type : 4
+}];
