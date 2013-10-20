887c6c3feab88f2d691cba233545a22fde4177f6
Miroslav Magda <magda.miroslav@gmail.com>
2013-10-20 15:48:17 +0200

    New communication "format" page.js <---> background.js <---> popup.js

--- extension/page/page.js
+++ extension/page/page.js
@@ -1,200 +1,187 @@
 /**
  * Document content loaded
  */
-//if (window != window.top) {
-if (false) {
-    console.log('iframe element');
-} else {
-    document.addEventListener("DOMContentLoaded", function() {
-        var videos, audios;
-        var context, masterNode, filter32, filter64, filter125, filter250, filter500, filter1000, filter2000, filter4000, filter8000, filter16000;
-        var initialized = false;
-        var eq = {};
-        eq.init = function() {
-            try {
-                context = new webkitAudioContext();
-                masterNode = context.createGainNode();
-                masterNode.gain.value = 1;
-                //mastervolume
-                //32,64,125,250,500,1K,2K,4K,8K,16K
-                var createFilter = function(f, t, g) {
-                    f = (f) ? f : 0;
-                    t = (t) ? t : 5;
-                    g = (g) ? g : 0;
-                    var filter = context.createBiquadFilter();
-                    filter.type = t;
-                    filter.gain.value = g;
-                    filter.Q.value = 1;
-                    filter.frequency.value = f;
-                    return filter;
-                };
-                filter32 = createFilter(32, 3);
-                filter64 = createFilter(64, 5);
-                filter125 = createFilter(125, 5);
-                filter250 = createFilter(250, 5);
-                filter500 = createFilter(500, 5);
-                filter1000 = createFilter(1000, 5);
-                filter2000 = createFilter(2000, 5);
-                filter4000 = createFilter(4000, 5);
-                filter8000 = createFilter(8000, 5);
-                filter16000 = createFilter(16000, 4);
-                // Change frequency to test
-                //console.log("videoTag", videoTag);
-                initialized = true;
-                console.log('eq.init', 'done');
-                //eq.attach();
-            } catch(e) {
-                console.error('eq.init', e);
-                //eate exception :)
-                //throw e;
-            }
 
-        };
-        eq.attach = function() {
-            //console.log('initialized', initialized);
-            if (!initialized) {
-                return;
-            }
-            try {
-                videos = document.getElementsByTagName('video');
-                audios = document.getElementsByTagName('audio');
-                /*var iframes = document.getElementsByTagName('iframe');
-                 for (var i = 0; i < iframes.length; i++) {
-                 var v = iframes[i].contentWindow.document.getElementsByTagName('video');
-                 var a = iframes[i].contentWindow.document.getElementsByTagName('audio');
-                 console.log(v, a);
-                 audios = (a.length > 0) ? audios.concat(a) : audios;
-                 videos = (v.length > 0) ? videos.concat(v) : videos;
-                 }*/
-                console.log('eq.attach', videos, audios);
-
-                //console.log('eq.attach', 'audios', audios);
-                var source = false;
-                var count = 0;
-                for (var i = 0; i < audios.length; i++) {
-                    if (audios[i].getAttribute('eq-attached') !== 'true') {
-                        source = context.createMediaElementSource(audios[i]);
-                        console.log(audios[i], source);
-                        source.connect(masterNode);
-                        masterNode.connect(filter32);
-                        filter32.connect(filter64);
-                        filter64.connect(filter125);
-                        filter125.connect(filter250);
-                        filter250.connect(filter500);
-                        filter500.connect(filter1000);
-                        filter1000.connect(filter2000);
-                        filter2000.connect(filter4000);
-                        filter4000.connect(filter8000);
-                        filter8000.connect(filter16000);
-                        filter16000.connect(context.destination);
-                        audios[i].setAttribute('eq-attached', 'true');
-                        count++;
-                    }
-                }
-                source = false;
-                //console.log('eq.attach', 'videos', videos);
-                for (var i = 0; i < videos.length; i++) {
-                    if (videos[i].getAttribute('eq-attached') !== 'true') {
-                        source = context.createMediaElementSource(videos[i]);
-                        console.log(videos[i], source);
-                        source.connect(masterNode);
-                        masterNode.connect(filter32);
-                        filter32.connect(filter64);
-                        filter64.connect(filter125);
-                        filter125.connect(filter250);
-                        filter250.connect(filter500);
-                        filter500.connect(filter1000);
-                        filter1000.connect(filter2000);
-                        filter2000.connect(filter4000);
-                        filter4000.connect(filter8000);
-                        filter8000.connect(filter16000);
-                        filter16000.connect(context.destination);
-                        videos[i].setAttribute('eq-attached', 'true');
-                        count++;
-                    }
-                }
-                if (count > 0) {
-                    console.log('eq.attach', 'Attached to ' + count + ' video/audio sources.');
-                }
-            } catch(e) {
-                //console.error('eq.attach', e);
-                //throw e;
-            }
-        };
-        eq.set = function(options) {
-            try {
-                if (masterNode) {
-                    masterNode.gain.value = options.masterVolume;
-                    filter32.gain.value = options.filter32;
-                    filter64.gain.value = options.filter64;
-                    filter125.gain.value = options.filter125;
-                    filter500.gain.value = options.filter500;
-                    filter1000.gain.value = options.filter1000;
-                    filter2000.gain.value = options.filter2000;
-                    filter4000.gain.value = options.filter4000;
-                    filter8000.gain.value = options.filter8000;
-                    filter16000.gain.value = options.filter16000;
-                }
-            } catch(e) {
-                console.error(e);
-            }
-        };
-        chrome.runtime.sendMessage({
-            action : 'get'
-        }, function(response) {
-            eq.set(response);
-        });
-        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-            //console.log(request);
-            if (request.action == 'set') {
-                //console.log('page.js', 'set', request.eqSettings);
-                eq.set(request.eqSettings);
-                sendResponse();
-            }
-            //return true;
-        });
-
-        //observe DOM
-        //http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-        //TODO: optimize for chrome (remove FF,IE support)
-        var observeDOM = (function() {
-            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver, eventListenerSupported = window.addEventListener;
-
-            return function(obj, callback) {
-                if (MutationObserver) {
-                    // define a new observer
-                    var obs = new MutationObserver(function(mutations, observer) {
-                        if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
-                            try {
-                                callback();
-                            } catch(e) {
-                                //nasty hack... :( ... eat exception
-                            }
-                        }
-                    });
-                    // have the observer observe foo for changes in children
-                    obs.observe(obj, {
-                        childList : true,
-                        subtree : true
-                    });
-                } else if (eventListenerSupported) {
-                    obj.addEventListener('DOMNodeInserted', callback, false);
-                    obj.addEventListener('DOMNodeRemoved', callback, false);
-                }
+document.addEventListener("DOMContentLoaded", function() {
+    var videos, audios;
+    var context, masterNode, filter32, filter64, filter125, filter250, filter500, filter1000, filter2000, filter4000, filter8000, filter16000;
+    var initialized = false;
+    var eq = {};
+    var filters = [];
+    eq.init = function() {
+        try {
+            context = new webkitAudioContext();
+            //masterNode = context.createGainNode();
+            //masterNode.gain.value = 1;
+            //mastervolume
+            //32,64,125,250,500,1K,2K,4K,8K,16K
+            var createFilter = function(f, t, g) {
+                f = (f) ? f : 0;
+                t = (t) ? t : 5;
+                g = (g) ? g : 0;
+                var filter = context.createBiquadFilter();
+                filter.type = t;
+                filter.gain.value = g;
+                filter.Q.value = 1;
+                filter.frequency.value = f;
+                return filter;
             };
-        })();
 
-        // Observe a specific DOM element:
-        observeDOM(document.getElementsByTagName('body')[0], function() {
-            var newVideos = document.getElementsByTagName('video');
-            var newAudios = document.getElementsByTagName('audio');
-            //console.log('observeDOM', newVideos.length, newAudios.length);
-            //if (newVideos.length !== videos.length || newAudios.length !== audios.length) {
-            eq.attach();
-            //}
+            for (var l = CONST.EQ.length, i = 0; i < l; i++) {
+                if (CONST.EQ[i].f) {
+                    //filter node
+                    filters.push(createFilter(CONST.EQ[i].f, CONST.EQ[i].type));
+                } else {
+                    //gain node
+                    filters.push(context.createGainNode());
+                    //filters[i].gain.value = 1;
+                }
+            }
+            filters[0].gain.value = 1;
+            console.log(filters);
+            /*
+             filter32 = createFilter(32, 3);
+             filter64 = createFilter(64, 5);
+             filter125 = createFilter(125, 5);
+             filter250 = createFilter(250, 5);
+             filter500 = createFilter(500, 5);
+             filter1000 = createFilter(1000, 5);
+             filter2000 = createFilter(2000, 5);
+             filter4000 = createFilter(4000, 5);
+             filter8000 = createFilter(8000, 5);
+             filter16000 = createFilter(16000, 4);
+             */
+            initialized = true;
+
+            console.log('eq.init', 'done');
+        } catch(e) {
+            console.error('eq.init', e);
+        }
+
+    };
+    eq.attach = function() {
+        //console.log('initialized', initialized);
+        //console.log('eq.attach', 'start');
+        if (!initialized) {
+            return;
+        }
+        try {
+            videos = document.getElementsByTagName('video');
+            audios = document.getElementsByTagName('audio');
+            var tags = [];
+            //stupid I know :(
+            for (var l = audios.length, i = 0; i < l; i++) {
+                tags.push(audios[i]);
+            }
+            for (var l = videos.length, i = 0; i < l; i++) {
+                tags.push(videos[i]);
+            }
+            //console.log('eq.attach', tags, videos, audios);
+            var source = false;
+            var count = 0;
+            for (var l = tags.length, i = 0; i < l; i++) {
+                //connect filters in chain
+                //console.log(tags[i], tags[i].getAttribute('eq-attached'));
+                if (tags[i].getAttribute('eq-attached') !== 'true') {
+                    source = context.createMediaElementSource(tags[i]);
+                    source.connect(filters[0]);
+                    for (var fl = filters.length, j = 0; j < fl - 1; j++) {
+                        //console.log(filters[j], filters[j+1]);
+                        filters[j].connect(filters[j + 1]);
+                    }
+                    filters[filters.length - 1].connect(context.destination);
+                    //set attribute to tag that it is already connected
+                    tags[i].setAttribute('eq-attached', 'true');
+                    count++;
+                }
+            }
+            //console.log('eq.attach', 'done');
+
+            if (count > 0) {
+                console.log('eq.attach', 'Attached to ' + count + ' video/audio sources.');
+            }
+        } catch(e) {
+            console.error('eq.attach', e);
+            //throw e;
+        }
+    };
+    /**
+     * Set filter values based on new options
+     * @param {Object} options
+     */
+    eq.set = function(options) {
+        try {
+            if (filters[0]) {
+                console.log(options);
+                for (var l = filters.length, i = 0; i < l; i++) {
+                    console.log(filters[i].gain.value + ' --> ' + options.eq[i].gain);
+                    filters[i].gain.value = options.eq[i].gain;
+                }
+            }
+        } catch(e) {
+            console.error(e);
+        }
+    };
+    //Get default values
+    chrome.runtime.sendMessage({
+        action : 'get'
+    }, function(response) {
+        eq.set({
+            eq : response.eq
         });
+    });
+    //Listen to changes
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        console.log(request);
+        if (request.action == 'set') {
+            //console.log('page.js', 'set', request.eqSettings);
+            eq.set({
+                eq : request.eq
+            });
+            sendResponse();
+        }
+        //return true;
+    });
 
-        eq.init();
+    //observe DOM
+    //http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+    var observeDOM = (function() {
+        var MutationObserver = window.MutationObserver, eventListenerSupported = window.addEventListener;
+        return function(obj, callback) {
+            if (MutationObserver) {
+                // define a new observer
+                var obs = new MutationObserver(function(mutations, observer) {
+                    if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+                        try {
+                            callback();
+                        } catch(e) {
+                            //nasty hack... :( ... eat exception
+                        }
+                    }
+                });
+                obs.observe(obj, {
+                    childList : true,
+                    subtree : true
+                });
+            } else if (eventListenerSupported) {
+                obj.addEventListener('DOMNodeInserted', callback, false);
+                obj.addEventListener('DOMNodeRemoved', callback, false);
+            }
+        };
+    })();
 
-    }, false);
-}
+    // Observe a specific DOM element:
+    observeDOM(document.getElementsByTagName('body')[0], function() {
+        var newVideos = document.getElementsByTagName('video');
+        var newAudios = document.getElementsByTagName('audio');
+        //console.log('observeDOM', newVideos.length, newAudios.length);
+        //if (newVideos.length !== videos.length || newAudios.length !== audios.length) {
+        eq.attach();
+        //}
+    });
+
+    //auto init
+
+    eq.init();
+    //eq.attach();
+}, false);
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,60 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('popup.js');
-    var eq = [{
-        label : '32',
-        f : 32,
-        gain : 0
-    }, {
-        label : '64',
-        f : 64,
-        gain : 0
-    }, {
-        label : '125',
-        f : 125,
-        gain : 0
-    }, {
-        label : '250',
-        f : 250,
-        gain : 0
-    }, {
-        label : '500',
-        f : 500,
-        gain : 0
-    }, {
-        label : '1k',
-        f : 1000,
-        gain : 0
-    }, {
-        label : '2k',
-        f : 2000,
-        gain : 0
-    }, {
-        label : '4k',
-        f : 4000,
-        gain : 0
-    }, {
-        label : '8k',
-        f : 8000,
-        gain : 0
-    }, {
-        label : '16k',
-        f : 16000,
-        gain : 0
-    }];
-    var eqSettings = {
-        masterVolume : 1,
-        filter32 : 0,
-        filter64 : 0,
-        filter125 : 0,
-        filter250 : 0,
-        filter500 : 0,
-        filter1000 : 0,
-        filter2000 : 0,
-        filter4000 : 0,
-        filter8000 : 0,
-        filter16000 : 0
-    };
-    console.log(eq);
+    logger.log('popup.js');
+    var eq = CONST.EQ;
+    logger.log(eq);
+
     function getValue(id) {
         return document.getElementById(id).value;
     }
@@ -65,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function getEqIndex(f) {
         for (var l = eq.length, i = 0; i < l; i++) {
-            if (eq[i].f + '' === f) {
+            if (eq[i].f && eq[i].f + '' === f) {
                 return i;
             }
         }
@@ -74,99 +22,82 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function onchange() {
         var slider = this.getAttribute('eq');
-        //console.log(slider);
         if (slider === 'master') {
             //master volume change
-            eqSettings.masterVolume = getValue('ch-eq-slider-master');
+            eq[0].gain = getValue('ch-eq-slider-0');
         } else {
             //eq settings change
             var index = getEqIndex(slider);
             var diff = this.value - eq[index].gain;
             eq[index].gain = this.value;
-            console.log(slider, diff, index);
-            for (var i = 1; i < 9; i++) {
+            logger.log(slider, diff, index);
+
+            for (var i = 1; i < 10; i++) {
                 diff = diff / 2;
-                if (eq[index - i]) {
+                if (eq[index - i] && eq[index - i].f) {
                     eq[index - i].gain = parseFloat(eq[index - i].gain, 10) + diff;
                 }
-                if (eq[index + i]) {
+                if (eq[index + i] && eq[index + i].f) {
                     eq[index + i].gain = parseFloat(eq[index + i].gain, 10) + diff;
                 }
             }
-            setValue('ch-eq-slider-32', eq[0].gain);
-            setValue('ch-eq-slider-64', eq[1].gain);
-            setValue('ch-eq-slider-125', eq[2].gain);
-            setValue('ch-eq-slider-250', eq[3].gain);
-            setValue('ch-eq-slider-500', eq[4].gain);
-            setValue('ch-eq-slider-1000', eq[5].gain);
-            setValue('ch-eq-slider-2000', eq[6].gain);
-            setValue('ch-eq-slider-4000', eq[7].gain);
-            setValue('ch-eq-slider-8000', eq[8].gain);
-            setValue('ch-eq-slider-16000', eq[9].gain);
 
-            eqSettings.filter32 = getValue('ch-eq-slider-32');
-            eqSettings.filter64 = getValue('ch-eq-slider-64');
-            eqSettings.filter125 = getValue('ch-eq-slider-125');
-            eqSettings.filter250 = getValue('ch-eq-slider-250');
-            eqSettings.filter500 = getValue('ch-eq-slider-500');
-            eqSettings.filter1000 = getValue('ch-eq-slider-1000');
-            eqSettings.filter2000 = getValue('ch-eq-slider-2000');
-            eqSettings.filter4000 = getValue('ch-eq-slider-4000');
-            eqSettings.filter8000 = getValue('ch-eq-slider-8000');
-            eqSettings.filter16000 = getValue('ch-eq-slider-16000');
+            setValue('ch-eq-slider-1', eq[1].gain);
+            setValue('ch-eq-slider-2', eq[2].gain);
+            setValue('ch-eq-slider-3', eq[3].gain);
+            setValue('ch-eq-slider-4', eq[4].gain);
+            setValue('ch-eq-slider-5', eq[5].gain);
+            setValue('ch-eq-slider-6', eq[6].gain);
+            setValue('ch-eq-slider-7', eq[7].gain);
+            setValue('ch-eq-slider-8', eq[8].gain);
+            setValue('ch-eq-slider-9', eq[9].gain);
+            setValue('ch-eq-slider-10', eq[10].gain);
 
         }
 
         //send message
         try {
-            //console.log('popup.js', eqSettings);
+            logger.log('popup.js', eq);
             chrome.runtime.sendMessage({
                 action : 'set',
-                eqSettings : eqSettings
+                eq : eq
             });
         } catch(e) {
-            // :)
+            // Psssst! Dont tell anyone :)
         }
     };
     var inputs = document.getElementsByTagName('input');
     for (var i = 0; i < inputs.length; i++) {
-        //console.log(inputs);
+        //logger.log(inputs);
         inputs[i].onchange = onchange;
     }
     //TODO: dont repeat yor self!
     document.getElementById('reset').onclick = function() {
         for (var i = 0; i < eq.length; i++) {
-            eq[i].gain = 0;
+            if (eq[i].f) {
+                eq[i].gain = 0;
+            } else {
+                eq[i].gain = 1;
+            }
         }
-        eqSettings.filter32 = getValue('ch-eq-slider-32');
-        setValue('ch-eq-slider-32', eq[0].gain);
-        setValue('ch-eq-slider-64', eq[1].gain);
-        setValue('ch-eq-slider-125', eq[2].gain);
-        setValue('ch-eq-slider-250', eq[3].gain);
-        setValue('ch-eq-slider-500', eq[4].gain);
-        setValue('ch-eq-slider-1000', eq[5].gain);
-        setValue('ch-eq-slider-2000', eq[6].gain);
-        setValue('ch-eq-slider-4000', eq[7].gain);
-        setValue('ch-eq-slider-8000', eq[8].gain);
-        setValue('ch-eq-slider-16000', eq[9].gain);
-
-        eqSettings.filter32 = getValue('ch-eq-slider-32');
-        eqSettings.filter64 = getValue('ch-eq-slider-64');
-        eqSettings.filter125 = getValue('ch-eq-slider-125');
-        eqSettings.filter250 = getValue('ch-eq-slider-250');
-        eqSettings.filter500 = getValue('ch-eq-slider-500');
-        eqSettings.filter1000 = getValue('ch-eq-slider-1000');
-        eqSettings.filter2000 = getValue('ch-eq-slider-2000');
-        eqSettings.filter4000 = getValue('ch-eq-slider-4000');
-        eqSettings.filter8000 = getValue('ch-eq-slider-8000');
-        eqSettings.filter16000 = getValue('ch-eq-slider-16000');
+        setValue('ch-eq-slider-0', eq[0].gain);
+        setValue('ch-eq-slider-1', eq[1].gain);
+        setValue('ch-eq-slider-2', eq[2].gain);
+        setValue('ch-eq-slider-3', eq[3].gain);
+        setValue('ch-eq-slider-4', eq[4].gain);
+        setValue('ch-eq-slider-5', eq[5].gain);
+        setValue('ch-eq-slider-6', eq[6].gain);
+        setValue('ch-eq-slider-7', eq[7].gain);
+        setValue('ch-eq-slider-8', eq[8].gain);
+        setValue('ch-eq-slider-9', eq[9].gain);
+        setValue('ch-eq-slider-10', eq[10].gain);
 
         //send message
         try {
-            //console.log('popup.js', eqSettings);
+            //logger.log('popup.js', eqSettings);
             chrome.runtime.sendMessage({
                 action : 'set',
-                eqSettings : eqSettings
+                eq : eq
             });
         } catch(e) {
             // :)
@@ -174,33 +105,11 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     try {
-        //TODO: change eqSettings object... (send directky eq object)
         chrome.storage.local.get(function(items) {
-            console.log(items, items['eqSettings']);
-            var eqSettings = items['eqSettings'];
-            setValue('ch-eq-slider-master', eqSettings.masterVolume);
-            setValue('ch-eq-slider-32', eqSettings.filter32);
-            setValue('ch-eq-slider-64', eqSettings.filter64);
-            setValue('ch-eq-slider-125', eqSettings.filter125);
-            setValue('ch-eq-slider-250', eqSettings.filter250);
-            setValue('ch-eq-slider-500', eqSettings.filter500);
-            setValue('ch-eq-slider-1000', eqSettings.filter1000);
-            setValue('ch-eq-slider-2000', eqSettings.filter2000);
-            setValue('ch-eq-slider-4000', eqSettings.filter4000);
-            setValue('ch-eq-slider-8000', eqSettings.filter8000);
-            setValue('ch-eq-slider-16000', eqSettings.filter16000);
-            eq[0].gain = eqSettings.filter32;
-            eq[1].gain = eqSettings.filter64;
-            eq[2].gain = eqSettings.filter125;
-            eq[3].gain = eqSettings.filter250;
-            eq[4].gain = eqSettings.filter500;
-            eq[5].gain = eqSettings.filter1000;
-            eq[6].gain = eqSettings.filter2000;
-            eq[7].gain = eqSettings.filter4000;
-            eq[8].gain = eqSettings.filter8000;
-            eq[9].gain = eqSettings.filter16000;
+            logger.log(items, items['eq']);
+            var eq = items['eq'];
         });
     } catch(e) {
-        // :)
+        // Psssst! Dont tell anyone :)
     }
 });
