1701ea558db77beac4c86d15e2f6968a5a6bb4a8
Miroslav Magda <magda.miroslav@gmail.com>
2013-12-26 09:03:40 +0100

    EQ settings (empty)

--- extension/common/const.js
+++ extension/common/const.js
@@ -9,9 +9,7 @@ var CONST = {};
  */
 CONST.EQ = [{
     label : 'master',
-    gain : 1,
-    channelCountMode : 'explicit',
-    channelCount : 2
+    gain : 1
 }, {
     label : '32',
     f : 32,
@@ -63,3 +61,100 @@ CONST.EQ = [{
     gain : 0,
     type : 4
 }];
+CONST.CONFIG = {
+    snap : false,
+    mono : false
+};
+PRESETS = [];
+PRESETS.push({
+    name : 'none',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Acoustic',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Bass Booster',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Bass Reducer',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Classical',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Dance',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Deep',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Electronic',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Flat',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Hip-Hop',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Jazz',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Latin',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Loudness',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Lounge',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Piano',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Pop',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'R&B',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Rock',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Small Speakers',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Spoken Word',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Treble Booster',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Treble Reducer',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
+PRESETS.push({
+    name : 'Vocal Booster',
+    gains : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+});
