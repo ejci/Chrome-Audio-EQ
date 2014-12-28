b47cc38b7a1b9bff4fbb98a72cc0a29d38108ff9
Miroslav Magda <magda.miroslav@gmail.com>
2014-12-28 23:18:47 +0100

    Design changes Preparation for custom presets

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,14 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+var eq = CONST.EQ;
+var config = CONST.config;
+
+var init = function(presets) {
 	try {
-		//logger.log('popup.js');
-		var eq = CONST.EQ;
-		var config = CONST.config;
-		var presets = PRESETS;
 		var px = (window.devicePixelRatio > 1) ? 2 : 1;
-		//px=1;
 
 		var canvas, context, inputs;
-		//logger.log(eq);
 
 		function getValue(id) {
 			return document.getElementById(id).value;
@@ -29,7 +26,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 		var propagateData = function() {
 			//send message
-			//logger.log('popup.js', eq, config);
 			try {
 				chrome.runtime.sendMessage({
 					action : 'set',
@@ -44,13 +40,13 @@ document.addEventListener("DOMContentLoaded", function() {
 		var prepareSliders = function() {
 			var canvas = document.createElement('canvas');
 			canvas.width = px * 30;
-			canvas.height = px * 101;
+			canvas.height = px * 120;
 			var context = canvas.getContext('2d');
 			context.beginPath();
-			context.strokeStyle = 'rgba(180,180,180,100)';
+			context.strokeStyle = 'rgb(170,170,170)';
 			context.lineWidth = px * 1;
-			var longer = [0, 50, 100];
-			var shorter = [10, 20, 30, 40, 60, 70, 80, 90];
+			var longer = [0, 59, 119];
+			var shorter = [10, 20, 30, 40, 50, 70, 80, 90, 100, 110];
 			for (var i = 0; i < longer.length; i++) {
 				context.moveTo(px * 2, px * longer[i] + context.lineWidth / 2);
 				context.lineTo(px * 10, px * longer[i] + context.lineWidth / 2);
@@ -68,44 +64,45 @@ document.addEventListener("DOMContentLoaded", function() {
 			var url = canvas.toDataURL('image/png');
 			var head = document.getElementsByTagName('head')[0];
 			var style = document.createElement('style');
-			style.innerHTML += '.ch-eq .sliders {';
+			style.innerHTML += '.controls-sliders .slider{';
 			style.innerHTML += 'background-image: url(' + url + ');';
-			style.innerHTML += 'background-size: 30px 101px;';
+			style.innerHTML += 'background-size: 30px 120px;';
 			style.innerHTML += '}';
 			head.appendChild(style);
 		};
 		var prepareChart = function() {
-			canvas = document.getElementById('ch-eq-chart');
+			canvas = document.getElementById('chart');
 			//330x40
-			canvas.style.width = 330;
-			canvas.style.height = 40;
-			canvas.width = px * 330;
+			console.log(px);
+			canvas.style.width = 315 + 'px';
+			canvas.style.height = 40 + 'px';
+			canvas.width = px * 315;
 			canvas.height = px * 40;
 			context = canvas.getContext('2d');
 			context.beginPath();
-			context.moveTo(px * 17, px * 20);
-			context.lineTo(px * 314, px * 20);
+			context.moveTo(px * 12, px * 20);
+			context.lineTo(px * 300, px * 20);
 			context.lineWidth = px * 0.2;
-			context.strokeStyle = 'rgb(200,200,200)';
+			context.strokeStyle = 'rgb(170,170,170)';
 			context.stroke();
 			context.beginPath();
 			for (var l = eq.length, i = 0; i < l; i++) {
-				context.moveTo(px * ((i * 33) + 17), px * 5);
-				context.lineTo(px * ((i * 33) + 17), px * 35);
+				context.moveTo(px * ((i * 32) + 12), px * 5);
+				context.lineTo(px * ((i * 32) + 12), px * 35);
 			}
 			context.lineWidth = px * 0.2;
-			context.strokeStyle = 'rgb(200,200,200)';
+			context.strokeStyle = 'rgb(170,170,170)';
 			context.stroke();
 			context.beginPath();
 			context.stroke();
 			context.font = px * 8 + 'px Rationale';
 			context.textAlign = 'right';
 			context.fillStyle = 'rgb(50,90,140)';
-			context.fillText('+12', px * 14, px * (6 + 3));
-			context.fillText('-12', px * 14, px * (40 - 3));
+			context.fillText('+12', px * 8, px * (6 + 3));
+			context.fillText('-12', px * 8, px * (40 - 3));
 			context.textAlign = 'left';
-			context.fillText('+12', px * 316, px * (6 + 3));
-			context.fillText('-12', px * 316, px * (40 - 3));
+			context.fillText('+12', px * 303, px * (6 + 3));
+			context.fillText('-12', px * 303, px * (40 - 3));
 			context.closePath();
 
 			refreshChart();
@@ -114,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			var points = [];
 			for (var l = eq.length, i = 1; i < l; i++) {
 				points.push({
-					x : ((i - 1) * 33) + 17,
+					x : ((i - 1) * 32) + 12,
 					y : 20 - (15 / 12) * eq[i].gain,
 					xc : 0,
 					xy : 0
@@ -128,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				context.quadraticCurveTo(px * points[i].x, px * points[i].y, px * xc, px * yc);
 			}
 			context.quadraticCurveTo(px * points[i].x, px * points[i].y, px * points[i + 1].x, px * points[i + 1].y);
-			context.lineWidth = px * 0.5;
+			context.lineWidth = px * 1;
 			context.strokeStyle = 'rgb(50,90,140)';
 			context.stroke();
 
@@ -144,7 +141,6 @@ document.addEventListener("DOMContentLoaded", function() {
 				var index = getEqIndex(slider);
 				var diff = this.value - eq[index].gain;
 				eq[index].gain = this.value;
-				//logger.log(slider, diff, index);
 
 				if (config.snap) {
 					for (var i = 1; i < 10; i++) {
@@ -176,8 +172,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 		inputs = document.getElementsByTagName('input');
 		for (var i = 0; i < inputs.length; i++) {
-			//logger.log(inputs);
 			inputs[i].onchange = onchange;
+			inputs[i].oninput = onchange;
+
 		}
 
 		//TODO: dont repeat yor self!
@@ -202,7 +199,9 @@ document.addEventListener("DOMContentLoaded", function() {
 			setValue('ch-eq-slider-10', eq[10].gain);
 
 			// When user presses reset, change it back to default value (stereo)
-			eq[0].channelCount = CONST.EQ[0].channelCount;
+			config.mono = false;
+			document.getElementById('channels').classList.remove('on');
+
 			prepareChart();
 			propagateData();
 
@@ -211,33 +210,41 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.getElementById('channels').onclick = function(ev) {
 			config.mono = !config.mono;
 			if (config.mono === true) {
-				document.getElementById('channels').childNodes[0].classList.add('on');
+				document.getElementById('channels').classList.add('on');
 			} else {
-				document.getElementById('channels').childNodes[0].classList.remove('on');
+				document.getElementById('channels').classList.remove('on');
 			}
-			//logger.log(config);
 			propagateData();
 		};
 
 		document.getElementById('snap').onclick = function(ev) {
 			config.snap = !config.snap;
 			if (config.snap === true) {
-				document.getElementById('snap').childNodes[0].classList.add('on');
+				document.getElementById('snap').classList.add('on');
 			} else {
-				document.getElementById('snap').childNodes[0].classList.remove('on');
+				document.getElementById('snap').classList.remove('on');
 			}
-			//logger.log(config);
 			propagateData();
 		};
 
 		document.getElementById('presets').onclick = function(ev) {
+
+			//load EQ presets
+			var presetsSelect = document.getElementById('presetsSelectDefaultLoad');
+			presetsSelect.innerHTML = '';
+			for (var l = presets.length, i = 0; i < l; i++) {
+				var option = document.createElement("option");
+				option.text = presets[i].name;
+				option.setAttribute('value', '' + i);
+				presetsSelect.appendChild(option, null);
+			}
 			var mousedownEvent = document.createEvent("MouseEvent");
 			mousedownEvent.initMouseEvent("mousedown");
 			document.getElementById('presetsSelect').dispatchEvent(mousedownEvent);
 		};
+
 		document.getElementById('presetsSelect').onchange = function(ev) {
 			var preset = presets[parseInt(ev.target.value, 10)];
-			console.log(preset);
 			for (var l = eq.length, i = 1; i < l; i++) {
 				eq[i].gain = preset.gains[i - 1];
 			}
@@ -252,8 +259,10 @@ document.addEventListener("DOMContentLoaded", function() {
 			setValue('ch-eq-slider-8', eq[8].gain);
 			setValue('ch-eq-slider-9', eq[9].gain);
 			setValue('ch-eq-slider-10', eq[10].gain);
+
 			prepareChart();
 			propagateData();
+
 		};
 
 		//intialization
@@ -262,7 +271,6 @@ document.addEventListener("DOMContentLoaded", function() {
 				chrome.runtime.sendMessage({
 					action : 'get'
 				}, function(response) {
-					//logger.log('popup.js', 'response', response);
 					eq = response.eq;
 					setValue('ch-eq-slider-0', eq[0].gain);
 					setValue('ch-eq-slider-1', eq[1].gain);
@@ -276,37 +284,48 @@ document.addEventListener("DOMContentLoaded", function() {
 					setValue('ch-eq-slider-9', eq[9].gain);
 					setValue('ch-eq-slider-10', eq[10].gain);
 
-					prepareChart();
-
 					config = response.config;
 					// CUSTOM: make sure toggle is checked
 					if (config.mono) {
-						document.getElementById('channels').childNodes[0].classList.add('on');
+						document.getElementById('channels').classList.add('on');
 					} else {
-						document.getElementById('channels').childNodes[0].classList.remove('on');
+						document.getElementById('channels').classList.remove('on');
 					}
 					if (config.snap) {
-						document.getElementById('snap').childNodes[0].classList.add('on');
+						document.getElementById('snap').classList.add('on');
 					} else {
-						document.getElementById('snap').childNodes[0].classList.remove('on');
+						document.getElementById('snap').classList.remove('on');
 					}
-
-					//load EQ presets
-					for (var l = presets.length, i = 0; i < l; i++) {
-						var option = document.createElement("option");
-						option.text = presets[i].name;
-						option.setAttribute('value', i);
-						document.getElementById('presetsSelect').add(option, null);
-					}
+					prepareChart();
 
 				});
 			}
+			prepareChart();
+			prepareSliders();
+
 		} catch(e) {
 			// Psssst! Dont tell anyone :)
 		}
-		prepareSliders();
 	} catch(e) {
-		logger.error('popup.js', e);
 		throw e;
 	}
+};
+document.addEventListener("DOMContentLoaded", function() {
+	if (chrome && chrome.storage) {
+		chrome.storage.sync.get('presets', function(data) {
+			if (data.presets) {
+				init(data.presets);
+			} else {
+				init(CONST.PRESETS);
+				//store it for next time
+				chrome.storage.sync.set({
+					presets : CONST.PRESETS
+				});
+			}
+		});
+	} else {
+		init(CONST.PRESETS);
+
+	}
+
 });
